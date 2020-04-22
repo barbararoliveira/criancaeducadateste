be98522da9f0619b06d614a425272a24f152ff56
Bárbara <babyr.oliveira@gmail.com>
2020-04-21 21:30:45 -0300

    Resolução dos exercícios propostos.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace criancaeducadateste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Me diga o que você pensa sobre o aluno que não faz a lição de casa.");
+            string frase = Console.ReadLine();
+            Console.WriteLine("Eu sou uma criança educada. No seu lugar eu diria:");
+            Console.WriteLine(frase.Replace("chato", "#@$%*!&") .Replace("chata", "#@$%*!&") .Replace("bobo", "#@$%*!&") .Replace("boba", "#@$%*!&") .Replace("feio", "#@$%*!&") .Replace("feia", "#@$%*!&") .Replace("boboca", "#@$%*!&") .Replace("bocó", "#@$%*!&") .Replace("palerma", "#@$%*!&") .Replace("paspalho", "#@$%*!&") .Replace("paspalha", "#@$%*!&") .Replace("tantã", "#@$%*!&") .Replace("panaca", "#@$%*!&") .Replace("pentelho", "#@$%*!&") .Replace("burro", "#@$%*!&") .Replace("burra", "#@$%*!&") .Replace("besta", "#@$%*!&"));
+        }
+    }
+}
